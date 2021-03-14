580f35f6ba8384d986238edaed85b51d121f5e11
chantalbenson <chantal.benson99@gmail.com>
2021-03-14 18:57:17 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
-<footer class="container-fluid"> <!-- div for the main image to stretch to the edge of the page -->
-      <div class="container"> <!-- div that holds the content in the middle of the page -->
-           <div class="row"> <!-- a row that gives us acess to the BS columns -->
-                <div class="col-md-6 text-center footer-content">
-               <p><?php bloginfo('name'); ?></p>
-            <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“tumblr”>
-            <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“rss” >
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+<p> <?php bloginfo('name'); ?></p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss” >
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt=“linkedin” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg" alt=“facebook” >
+            </div>
 
-          </div>
-          
-          <div class="col-md-6 text-center footer-content">
-               <p>Terms and Conditions</p> 
-     <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“linkedin”>
-     <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“facebook”>
-               </div>
-          </div>
-          
+        </div>
     </div>
-            
-      </footer>
+</footer>
+
+</body>
+</html>
 <?php wp_footer(); ?>
