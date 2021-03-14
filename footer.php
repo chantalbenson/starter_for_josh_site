1861f1086a5898199802a44b314dbf3e4eb15a84
chantalbenson <chantal.benson99@gmail.com>
2021-03-14 18:50:32 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,22 +3,19 @@
            <div class="row"> <!-- a row that gives us acess to the BS columns -->
                 <div class="col-md-6 text-center footer-content">
                <p><?php bloginfo('name'); ?></p>
-            <img src="images/tumblr.png" alt=“tumblr”>
-            <img src="images/rss.png" alt=“rss” >
+            <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“tumblr”>
+            <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“rss” >
 
           </div>
           
           <div class="col-md-6 text-center footer-content">
                <p>Terms and Conditions</p> 
-     <img src="images/linkedin.png" alt=“linkedin”>
-     <img src="images/facebook.png" alt=“facebook”>
+     <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“linkedin”>
+     <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“facebook”>
                </div>
           </div>
           
     </div>
             
       </footer>
-      
-  </body>
-</html> 
 <?php wp_footer(); ?>
