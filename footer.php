3ffe05ff76c21cf724bd6d26b6a2fe577783b519
chantalbenson <chantal.benson99@gmail.com>
2021-03-14 16:50:04 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="container-fluid"> <!-- div for the main image to stretch to the edge of the page -->
+      <div class="container"> <!-- div that holds the content in the middle of the page -->
+           <div class="row"> <!-- a row that gives us acess to the BS columns -->
+                <div class="col-md-6 text-center footer-content">
+               <p><?php bloginfo('name'); ?></p>
+            <img src="images/tumblr.png" alt=“tumblr”>
+            <img src="images/rss.png" alt=“rss” >
+
+          </div>
+          
+          <div class="col-md-6 text-center footer-content">
+               <p>Terms and Conditions</p> 
+     <img src="images/linkedin.png" alt=“linkedin”>
+     <img src="images/facebook.png" alt=“facebook”>
+               </div>
+          </div>
+          
+    </div>
+            
+      </footer>
+      
+  </body>
+</html> 
+<?php wp_footer(); ?>
