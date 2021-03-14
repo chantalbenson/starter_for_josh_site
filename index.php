6646ce75ac5049bbd4bc35db0bfc31efb2321971
chantalbenson <chantal.benson99@gmail.com>
2021-03-14 18:08:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -33,8 +33,7 @@
                 </div> <!--                row-->
                 
                 <div class="col-md-4 article-cent">
-    <img class="img-responsive" alt=“model” src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg
-">
+    <img class="img-responsive" alt=“model” src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg">
     <h4>A MODEL</h4>
     <p class="date">Friday,November 10. 2019 </p>
     <p>STS has never been shy of seeking new terrains of investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session. We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.  </p>
