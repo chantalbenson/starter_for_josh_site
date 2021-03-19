01ad1e4a332a079fcfe709b8898abc09e5f4c8e2
chantalbenson <chantal.benson99@gmail.com>
2021-03-19 16:19:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -21,9 +21,6 @@ wp_reset_query();?>
       </div><!--  container-->
     </section>
 
-
-
-
     <section class="container-fluid ">
         <div class="container">
             <div class="row"><!--  a row that gives us access to the BS columns-->
@@ -31,7 +28,7 @@ wp_reset_query();?>
                     <h3>LATEST ARTICLES</h3>
                 </div>
                 <div class="col-md-4 article-cent">
-                    <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/game.jpg">
+                    <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/portait.jpg">
                     <h4>A PORTRAIT</h4>
                                        <p class="date">12 December 2020</p>
                     <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -39,7 +36,7 @@ wp_reset_query();?>
                                <p class="readmore">CONTINUE READING</p>
                 </div>
                 <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
+                    <img class="img-responsive" alt=“model” src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/model.jpg">
                     <h4>A MODEL</h4>
                                         <p class="date">12 December 2020</p>
                     <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -47,7 +44,7 @@ wp_reset_query();?>
                                    <p class="readmore">CONTINUE READING</p>
                 </div>
                 <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
+                <img class="img-responsive" alt=“gamedesign” src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/gamedesign.jpg">
 
                     <h4>A GAMES DESIGN</h4>
                                         <p class="date">12 December 2020</p>
