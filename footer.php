60c1e7b6e5ce8d089d5eda481f1c6d17d4f1524f
chantalbenson <chantal.benson99@gmail.com>
2021-03-19 16:51:51 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-md-6 text-center footer-content">
 <p> <?php bloginfo('name'); ?></p>
-                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/tumblr.jpg" alt=“tumblr” >
-                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/rss.jpg" alt=“rss” >
+                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/tumblr.png" alt=“tumblr” >
+                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/rss.png" alt=“rss” >
             </div>
             <div class="col-md-6 text-center footer-content">
                 <p>Terms and Conditions</p>
-                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/linkedin.jpg" alt=“linkedin” >
-                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/facebook.jpg" alt=“facebook” >
+                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/linkedin.png" alt=“linkedin” >
+                <img src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/facebook.png" alt=“facebook” >
             </div>
 
         </div>
