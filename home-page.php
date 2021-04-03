d446ac9b478e7508b8d5822473d5886c3d06d830
chantalbenson <chantal.benson99@gmail.com>
2021-04-03 14:52:46 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,36 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+?>
+
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
+        <div class="container">
+
+
+            <div class="blocker"></div>
+
+      </div><!--  container-->
+    </section>
+
+    <section class="container-fluid ">
+        <div class="container">
+            <div class="row"><!--  a row that gives us access to the BS columns-->
+                       <div class="article-title text-center">
+                    <h3>LATEST ARTICLES</h3>
+                </div>
+                <div class="col-md-4 article-cent">
+                
+                </div>
+                <div class="col-md-4 article-cent">               
+
+                </div>
+                <div class="col-md-4 article-cent">
+                </div>
+                
+                    </div><!-- row-->
+            </div><!-- container-->
+        
+    </section><!-- container-fluid-->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
