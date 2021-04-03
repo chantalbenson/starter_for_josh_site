8a4d3086af9aeb75801735a8a836dfb52f28483d
chantalbenson <chantal.benson99@gmail.com>
2021-04-03 16:24:40 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -22,10 +22,16 @@
         <div class="container">
             <div class="row"><!--  a row that gives us access to the BS columns-->
                        <div class="article-title text-center">
-                    <h3>LATEST ARTICLES</h3>
+                    <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
                 </div>
-                <div class="col-md-4 article-cent">
-                
+
+<div class="col-md-4 article-cent">
+                    
+<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+<h4><?php the_field('articletitle'); ?></h4>
+<p><?php the_field('articleexcerpt'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+                                            
                 </div>
                 <div class="col-md-4 article-cent">               
 
